132e085210be4c260281debcaea92eb344aa33af
SimSimSim <leivaprogramming@gmail.com>
2024-04-29 15:11:04 -0400

    refacto update stock service

--- app/policies/admin/stock_policy.rb
+++ app/policies/admin/stock_policy.rb
@@ -5,6 +5,7 @@ module Admin
     def update_prices?
       user.admin?
     end
+
     def permitted_attributes
       %i[name symbol price status dividend yield distribution position api_symbol]
     end
